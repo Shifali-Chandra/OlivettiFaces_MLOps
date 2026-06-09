5ee5935d470d5114f63b07fc807adf9bdea55b56
Shifali Chandra <shifali.chandra20@gmail.com>
2026-06-10 02:49:22 +0530

    add image prediction logic

--- docker_cicd/app.py
+++ docker_cicd/app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from PIL import Image
+import numpy as np
+import joblib
 
 app = Flask(__name__)
+model = joblib.load("savedmodel.pth")
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    img = Image.open(request.files["image"]).convert("L")
+    img = img.resize((64, 64))
+    img = np.array(img) / 255.0
+    img = img.reshape(1, -1)
+    pred = model.predict(img)[0]
+
+    return render_template("index.html", prediction=pred)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
